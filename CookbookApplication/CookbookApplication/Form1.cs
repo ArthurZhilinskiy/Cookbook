5c303507b4c86cfa3c695c908015e8ae9facac8a
Arthur Zhilinskiy <zjilka22@mail.ru>
2018-12-10 00:00:31 +0300

    Connection to DB is OK

--- CookbookApplication/CookbookApplication/Form1.cs
+++ CookbookApplication/CookbookApplication/Form1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace CookbookApplication
 {
     public partial class MainApplicationForm : MetroFramework.Forms.MetroForm
     {
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + Application.StartupPath + @"\CookBook.mdf;Integrated Security = True; Connect Timeout = 30";
         public MainApplicationForm()
         {
             InitializeComponent();
@@ -24,9 +26,44 @@ namespace CookbookApplication
 
         private void MainApplicationForm_Load(object sender, EventArgs e)
         {
-            Carts cart = new Carts();
-            cart.metroTile1.Click += new System.EventHandler(cart_Click);
-            flowLayoutPanel1.Controls.Add(cart);
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                MessageBox.Show("OK");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                connection.Close();
+            }
+            //Carts cart = new Carts();
+            ////загружаем картинку в контрол
+            //cart.metroTile1.TileImage = Image.FromFile(getImage()); 
+            //cart.metroTile1.Click += new System.EventHandler(cart_Click);
+            //flowLayoutPanel1.Controls.Add(cart);
+        }
+
+        //метод получает путь к картинк из БД
+        private string getImage()
+        {
+            //SqlConnection connection = new SqlConnection(connectionString);
+            //try
+            //{
+            //    connection.Open();
+            //    MessageBox.Show("OK");
+            //}
+            //catch (Exception e)
+            //{
+            //    MessageBox.Show(e.Message);
+            //    connection.Close();
+            //}
+            
+            //SqlCommand command = new SqlCommand("SELECT ", connection);
+            //SqlDataReader reader = command.ExecuteReader();
+
+            string a = "";
+            return a;
         }
 
         private void cart_Click(object sender, EventArgs e)
@@ -34,7 +71,6 @@ namespace CookbookApplication
             FormWithRecipe formWithRecipe = new FormWithRecipe();
             string temp = sender.ToString();
             formWithRecipe.Text = temp.Substring(41);
-            formWithRecipe.metroLabel1.Focus();
             formWithRecipe.Show();
         }
     }
