13e84174da1b65a07d3f598fa5332b29833422dd
Arthur Zhilinskiy <zjilka22@mail.ru>
2018-12-09 16:51:37 +0300

    Added images to buttons

--- CookbookApplication/CookbookApplication/Form1.Designer.cs
+++ CookbookApplication/CookbookApplication/Form1.Designer.cs
@@ -29,11 +29,11 @@
         private void InitializeComponent()
         {
             this.mLeftSidePanel = new MetroFramework.Controls.MetroPanel();
-            this.mButtonAdd = new MetroFramework.Controls.MetroTile();
-            this.mButtonFind = new MetroFramework.Controls.MetroTile();
-            this.mButtonFilter = new MetroFramework.Controls.MetroTile();
             this.mListDishes = new MetroFramework.Controls.MetroPanel();
             this.mRecipesPanel = new MetroFramework.Controls.MetroPanel();
+            this.mButtonFilter = new MetroFramework.Controls.MetroTile();
+            this.mButtonFind = new MetroFramework.Controls.MetroTile();
+            this.mButtonAdd = new MetroFramework.Controls.MetroTile();
             this.mLeftSidePanel.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,36 +55,6 @@
             this.mLeftSidePanel.VerticalScrollbarBarColor = true;
             this.mLeftSidePanel.VerticalScrollbarHighlightOnWheel = false;
             this.mLeftSidePanel.VerticalScrollbarSize = 10;
-            // 
-            // mButtonAdd
-            // 
-            this.mButtonAdd.Location = new System.Drawing.Point(5, 27);
-            this.mButtonAdd.Name = "mButtonAdd";
-            this.mButtonAdd.Size = new System.Drawing.Size(50, 50);
-            this.mButtonAdd.Style = MetroFramework.MetroColorStyle.Blue;
-            this.mButtonAdd.TabIndex = 2;
-            this.mButtonAdd.Text = "Add";
-            this.mButtonAdd.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            // 
-            // mButtonFind
-            // 
-            this.mButtonFind.Location = new System.Drawing.Point(5, 83);
-            this.mButtonFind.Name = "mButtonFind";
-            this.mButtonFind.Size = new System.Drawing.Size(50, 50);
-            this.mButtonFind.Style = MetroFramework.MetroColorStyle.Blue;
-            this.mButtonFind.TabIndex = 2;
-            this.mButtonFind.Text = "Find";
-            this.mButtonFind.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            // 
-            // mButtonFilter
-            // 
-            this.mButtonFilter.Location = new System.Drawing.Point(5, 139);
-            this.mButtonFilter.Name = "mButtonFilter";
-            this.mButtonFilter.Size = new System.Drawing.Size(50, 50);
-            this.mButtonFilter.Style = MetroFramework.MetroColorStyle.Blue;
-            this.mButtonFilter.TabIndex = 2;
-            this.mButtonFilter.Text = "Filter";
-            this.mButtonFilter.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             // 
             // mListDishes
             // 
@@ -120,6 +90,48 @@
             this.mRecipesPanel.VerticalScrollbarBarColor = true;
             this.mRecipesPanel.VerticalScrollbarHighlightOnWheel = false;
             this.mRecipesPanel.VerticalScrollbarSize = 10;
+            // 
+            // mButtonFilter
+            // 
+            this.mButtonFilter.ForeColor = System.Drawing.Color.White;
+            this.mButtonFilter.Location = new System.Drawing.Point(5, 139);
+            this.mButtonFilter.Name = "mButtonFilter";
+            this.mButtonFilter.Size = new System.Drawing.Size(50, 50);
+            this.mButtonFilter.Style = MetroFramework.MetroColorStyle.Silver;
+            this.mButtonFilter.TabIndex = 2;
+            this.mButtonFilter.Text = "Filter";
+            this.mButtonFilter.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.mButtonFilter.TileImage = global::CookbookApplication.Properties.Resources.Filter;
+            this.mButtonFilter.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.mButtonFilter.UseTileImage = true;
+            // 
+            // mButtonFind
+            // 
+            this.mButtonFind.ForeColor = System.Drawing.Color.White;
+            this.mButtonFind.Location = new System.Drawing.Point(5, 83);
+            this.mButtonFind.Name = "mButtonFind";
+            this.mButtonFind.Size = new System.Drawing.Size(50, 50);
+            this.mButtonFind.Style = MetroFramework.MetroColorStyle.Silver;
+            this.mButtonFind.TabIndex = 2;
+            this.mButtonFind.Text = "Find";
+            this.mButtonFind.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.mButtonFind.TileImage = global::CookbookApplication.Properties.Resources.Find;
+            this.mButtonFind.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.mButtonFind.UseTileImage = true;
+            // 
+            // mButtonAdd
+            // 
+            this.mButtonAdd.ForeColor = System.Drawing.Color.White;
+            this.mButtonAdd.Location = new System.Drawing.Point(5, 27);
+            this.mButtonAdd.Name = "mButtonAdd";
+            this.mButtonAdd.Size = new System.Drawing.Size(50, 50);
+            this.mButtonAdd.Style = MetroFramework.MetroColorStyle.Silver;
+            this.mButtonAdd.TabIndex = 2;
+            this.mButtonAdd.Text = "Add";
+            this.mButtonAdd.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.mButtonAdd.TileImage = global::CookbookApplication.Properties.Resources.Add1;
+            this.mButtonAdd.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.mButtonAdd.UseTileImage = true;
             // 
             // MainApplicationForm
             // 
