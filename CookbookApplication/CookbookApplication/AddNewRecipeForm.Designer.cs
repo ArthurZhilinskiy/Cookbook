f108a27acfdebdaf2bb008f18a43a1b9c5fe5ee0
Arthur Zhilinskiy <zjilka22@mail.ru>
2018-12-10 11:40:01 +0300

    Update README.md

---
+++ CookbookApplication/CookbookApplication/AddNewRecipeForm.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace CookbookApplication
+{
+    partial class AddNewRecipeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.metroTile2 = new MetroFramework.Controls.MetroTile();
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.rtbSteps = new System.Windows.Forms.RichTextBox();
+            this.rtbIngredients = new System.Windows.Forms.RichTextBox();
+            this.metroTile3 = new MetroFramework.Controls.MetroTile();
+            this.rtbName = new System.Windows.Forms.RichTextBox();
+            this.rtbPrice = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.Location = new System.Drawing.Point(538, 425);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(93, 25);
+            this.metroTile1.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroTile1.TabIndex = 0;
+            this.metroTile1.Text = "Добавить";
+            this.metroTile1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 276);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(115, 19);
+            this.metroLabel1.TabIndex = 2;
+            this.metroLabel1.Text = "Название блюда:";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(23, 327);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(94, 19);
+            this.metroLabel2.TabIndex = 2;
+            this.metroLabel2.Text = "Ингредиенты:";
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(329, 60);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(88, 19);
+            this.metroLabel3.TabIndex = 2;
+            this.metroLabel3.Text = "Как готовить:";
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.Location = new System.Drawing.Point(206, 276);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(44, 19);
+            this.metroLabel4.TabIndex = 2;
+            this.metroLabel4.Text = "Цена:";
+            // 
+            // metroTile2
+            // 
+            this.metroTile2.Location = new System.Drawing.Point(23, 425);
+            this.metroTile2.Name = "metroTile2";
+            this.metroTile2.Size = new System.Drawing.Size(93, 25);
+            this.metroTile2.Style = MetroFramework.MetroColorStyle.Silver;
+            this.metroTile2.TabIndex = 0;
+            this.metroTile2.Text = "Отмена";
+            this.metroTile2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.AutoSize = true;
+            this.metroLabel5.Location = new System.Drawing.Point(23, 60);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(98, 19);
+            this.metroLabel5.TabIndex = 3;
+            this.metroLabel5.Text = "Изображение:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(23, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 150);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // rtbSteps
+            // 
+            this.rtbSteps.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.rtbSteps.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbSteps.Location = new System.Drawing.Point(329, 85);
+            this.rtbSteps.Name = "rtbSteps";
+            this.rtbSteps.Size = new System.Drawing.Size(302, 334);
+            this.rtbSteps.TabIndex = 5;
+            this.rtbSteps.Text = "";
+            // 
+            // rtbIngredients
+            // 
+            this.rtbIngredients.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.rtbIngredients.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbIngredients.Location = new System.Drawing.Point(23, 350);
+            this.rtbIngredients.Name = "rtbIngredients";
+            this.rtbIngredients.Size = new System.Drawing.Size(300, 69);
+            this.rtbIngredients.TabIndex = 6;
+            this.rtbIngredients.Text = "";
+            // 
+            // metroTile3
+            // 
+            this.metroTile3.Location = new System.Drawing.Point(23, 241);
+            this.metroTile3.Name = "metroTile3";
+            this.metroTile3.Size = new System.Drawing.Size(300, 25);
+            this.metroTile3.Style = MetroFramework.MetroColorStyle.Silver;
+            this.metroTile3.TabIndex = 0;
+            this.metroTile3.Text = "Выбрать изображение";
+            this.metroTile3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // rtbName
+            // 
+            this.rtbName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.rtbName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbName.Location = new System.Drawing.Point(23, 298);
+            this.rtbName.Multiline = false;
+            this.rtbName.Name = "rtbName";
+            this.rtbName.Size = new System.Drawing.Size(177, 18);
+            this.rtbName.TabIndex = 7;
+            this.rtbName.Text = "";
+            // 
+            // rtbPrice
+            // 
+            this.rtbPrice.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.rtbPrice.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbPrice.Location = new System.Drawing.Point(206, 298);
+            this.rtbPrice.Multiline = false;
+            this.rtbPrice.Name = "rtbPrice";
+            this.rtbPrice.Size = new System.Drawing.Size(117, 18);
+            this.rtbPrice.TabIndex = 7;
+            this.rtbPrice.Text = "";
+            // 
+            // AddNewRecipeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 473);
+            this.Controls.Add(this.rtbPrice);
+            this.Controls.Add(this.rtbName);
+            this.Controls.Add(this.rtbIngredients);
+            this.Controls.Add(this.rtbSteps);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.metroLabel5);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel4);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroTile3);
+            this.Controls.Add(this.metroTile2);
+            this.Controls.Add(this.metroTile1);
+            this.MaximizeBox = false;
+            this.Name = "AddNewRecipeForm";
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Green;
+            this.Text = "Добавление нового рецепта";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTile metroTile1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroTile metroTile2;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox rtbSteps;
+        private System.Windows.Forms.RichTextBox rtbIngredients;
+        private MetroFramework.Controls.MetroTile metroTile3;
+        private System.Windows.Forms.RichTextBox rtbName;
+        private System.Windows.Forms.RichTextBox rtbPrice;
+    }
+}
